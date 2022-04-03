d8781b0e911a77d0c58c0d57c2548d7e5d5a5859
James R. Wilcox <wilcoxjay@gmail.com>
2022-04-02 23:06:32 -0700

    hw2 handout

---
+++ hw2/README.md
@@ -0,0 +1,17 @@
+# CSE 341 HW2
+
+## OCaml setup
+
+If you set up your OCaml environment before Friday, April 1, 2022, please run
+
+```
+opam install ppx_inline_test
+```
+
+Otherwise, if you have not yet set up your OCaml environment, you can ignore the
+above and follow the [software setup
+instructions](https://courses.cs.washington.edu/courses/cse341/22sp/resources/software-setup.html#ocaml).
+
+## Handout
+
+The problems are described in [this PDF](https://courses.cs.washington.edu/courses/cse341/22sp/assignments/hw2.pdf) from the website.
---
+++ hw2/hw2.ml
@@ -0,0 +1,20 @@
+(* CSE 341, Homework 2, Provided Code *)
+
+(* Use these functions to extract parts of a date *)
+let fst3 (x,_,_) = x (* gets the first element of a triple *)
+let snd3 (_,x,_) = x (* gets the second element of a triple *)
+let thd3 (_,_,x) = x (* gets the third element of a triple *)
+
+(* TODO: Complete the 12 function bindings described in the
+   assignment. For the first two problems, we have given you the
+   *correct* first line and an *incorrect* function body. *)
+
+(* 1 *)
+let is_older ((date1 : int * int * int), (date2 : int * int * int)) =
+  false (* TODO: actually implement is_older *)
+
+(* 2 *)
+let rec number_in_month ((dates : (int * int * int) list), (month : int)) =
+  0 (* TODO: actually implement number_in_month *)
+
+(* TODO: continue for problem 3 and onward here *)
